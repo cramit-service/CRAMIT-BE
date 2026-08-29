5bfd63fdba3fa942a8ef60214490aea7199cd34d
jungini1123 <jungin070@gmail.com>
2026-08-29 22:06:46 +0900

    refactor: 사용하지 않는 import문 제거 (#14)

--- src/main/java/com/cramit/domain/week/WeekUpdateRequest.java
+++ src/main/java/com/cramit/domain/week/WeekUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.cramit.domain.week;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
