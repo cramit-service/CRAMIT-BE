51cb37770abb491981d874414e9ef0db80dd6232
jungini1123 <jungin070@gmail.com>
2026-09-02 21:59:48 +0900

    feat: 사용하지 않는 import문 삭제 취소 (#16)

--- src/test/java/com/cramit/domain/todo/TodoServiceTest.java
+++ src/test/java/com/cramit/domain/todo/TodoServiceTest.java
@@ -23,6 +23,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
