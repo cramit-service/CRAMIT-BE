287e94423031e1fd808a05dc7e42c3c18af9120f
jungini1123 <jungin070@gmail.com>
2026-09-07 22:49:26 +0900

    refactor: 사용되지 않는 import문 제거 (#27)

--- src/main/java/com/cramit/domain/share/MemberLectureRepository.java
+++ src/main/java/com/cramit/domain/share/MemberLectureRepository.java
@@ -3,15 +3,12 @@ package com.cramit.domain.share;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MemberLectureRepository extends JpaRepository<MemberLecture, Long> {
 
     List<MemberLecture> findByLectureId(Long lectureId);
 
     List<MemberLecture> findByMemberId(Long memberId);
-
-    Optional<MemberLecture> findByLectureIdAndMemberId(Long lectureId, Long memberId);
 
     boolean existsByLectureIdAndMemberId(Long lectureId, Long memberId);
 }
